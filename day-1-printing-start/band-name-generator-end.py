b7b02106c3f4bf60a78cd9443046fab51617aa6b
chrisfederico1 <chris.federico@yahoo.com>
2023-01-12 15:42:42 -0600

    Added comments

--- day-1-printing-start/band-name-generator-end.py
+++ day-1-printing-start/band-name-generator-end.py
@@ -1,5 +1,8 @@
 # Present Title line
 print("Welcome to the Band Name Generator.")
+# Get input for City notice the \n which is a new line
 city = input("What's the name of the city you grew up in?\n")
+# Get input for pet
 pet = input("What's your pet's name?\n")
+# Print out city and Pet as a name for band
 print("Your band name could be " + city + " " + pet)
