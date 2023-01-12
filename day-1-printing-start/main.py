1a51ad5b4da4c925b98797777c78e011631ba5df
chrisfederico1 <chris.federico@yahoo.com>
2023-01-12 14:35:55 -0600

    Starting 100 day python challenge

--- day-1-printing-start/main.py
+++ day-1-printing-start/main.py
@@ -1,1 +1,43 @@
-print("Hello World!")
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Writing on one line with the \n character
+print("Hi Chris!\nHow are you today")
+
+print()
+print()
+
+# String Concatination with the + string. Notice the different ways
+# for string concatination
+print("Hello" + " Chris")
+print("Hello " + "Chris")
+print("Hello" + " " + "Chris")
+
+print()
+print()
+# Day 1 String Manipulation
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " + '"+"' + " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+# Getting input from the user using input filter
+print()
+# input("What is your name?")
+print("Hello " + input("What is your name?"))
+
+print()
+print()
+
+print("This is an example of taking input and finding the length of a string")
+
+# Get Name and tell user how many characters their name is
+print(len(input("What is your name? ")))
+
+# Variables Starting
+print("Using variables in this section")
+name = input("What is your name? ")
+print(name)
