815da9721fccc2368188dd4598335c190066ccd4
chrisfederico1 <chris.federico@yahoo.com>
2023-01-13 16:44:27 -0600

    Day 2 of coding challenge

---
+++ day-2-Data-Types/main.py
@@ -0,0 +1,83 @@
+# Data Types
+
+# Strings
+
+print("Hello"[4])
+
+# Interger
+123 + 567
+
+# Float 1.456
+
+# boolean
+# True or False
+
+# If you need to change data to a different use () in front of variable
+# example
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print("Your name contains " + new_num_char + " characters")
+
+# a = float(123)
+# print(type(a))
+# two_digit_number = input("Type a two digit number: ")
+# first_num = int(two_digit_number[0])
+# second_num = int(two_digit_number[1])
+# 
+# print(first_num + second_num)
+
+# Mathematical Operations
+
+7 - 4
+5 + 10
+7 * 8
+2 ** 3
+
+# BMI calculator
+# 🚨 Don't change the code below 👇
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+# result = int(weight) / (float(height) ** 2)
+
+# print(int(result))
+
+# Number manipulation and F string in Python 
+# Use round function to round to nearest int
+
+print(8/3)
+
+
+print(8//3)
+
+# f string
+# Convert everything
+# Example 
+score = 0
+height = 1.8
+isWinning = True
+
+# f-string
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+
+# using a max age of 90 tell the user how many days,weeks and months remaining
+# # 🚨 Don't change the code below 👇
+# age = input("What is your current age? ")
+# # 🚨 Don't change the code above 👆
+# 
+# #Write your code below this line 👇
+# 
+# max_age = 90
+# 
+# # subract age from max_age to get years left
+# years_left = max_age - int(age)
+# months_left = years_left * 12
+# weeks_left = years_left * 52
+# days_left = years_left * 365
+# 
+# print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
+
+
