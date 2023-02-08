bf2073a929d71d6472862f333e1c18e7b8035abb
chrisfederico1 <chris.federico@yahoo.com>
2023-02-08 17:57:18 -0600

    Coffeemachine work

---
+++ day-15-local-development/coffeemachine/main.py
@@ -0,0 +1,45 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# keep track of coffee machine to turn it off
+turnoff = False
+
+# create our while loop
+while not turnoff:
+    # prompt user by asking what drink would they like
+    drink = input("What would you like? (espresso/latte/cappuccino): ")
+    if drink == "off":
+        turnoff = True
+    elif drink == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
