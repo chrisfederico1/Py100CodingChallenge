11a4437a2db8688753e0f359dce18f031166bdf7
chrisfederico1 <chris.federico@yahoo.com>
2023-03-14 13:48:08 -0500

    Adding high score to snake program

--- day-20-Build-the-snake-part-1/venv/scoreboard.py
+++ day-20-Build-the-snake-part-1/venv/scoreboard.py
@@ -11,12 +11,19 @@ class Scoreboard(Turtle):
         self.penup()
         self.score = 0
         self.color("white")
+        with open(file="data.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(file="data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
 
 
     def increase_score(self):
@@ -25,4 +32,5 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
